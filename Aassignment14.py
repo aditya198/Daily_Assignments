ee7aa5ff50b51ecd3d92f9ce0cea2e7e7b02710f
Aditya Batra <42377521+aditya198@users.noreply.github.com>
2018-09-06 01:38:11 +0530

    Add files via upload

--- Aassignment14.py
+++ Aassignment14.py
@@ -11,26 +11,26 @@ x=int(input("Enter the range: "))
 li = [i for i in range(1,x) if all(i%y!=0 for y in range(2,i))]
 print(li)
 
-#Q.3)
+#Q.3 )
 
 #A List Comprehension is like the plain range function, executes immediately and returns a list.
 #A Generator Expression returns and object that can be iterated over.
 #The comparision is not perfect because in an object returned by the generator expression we cannot access an element by index.
 #The difference between the two kinds of expressions is that the List comprehension is enclosed in square brackets [] while the Generator expression is enclosed in plain parentheses ()
 
-#Q.4)
+#Q.4 )
 
 Celsius=[39.2, 36.5, 37.3, 37.8]
 far=list(map(lambda x:(x*1.8)+32,Celsius))
 print(far)
 
-#Q.5)
+#Q.5 )
 
 li=[5,12,15,3,13]
 li_sq=list(map(lambda i: i*i,li))
 print(li_sq)
 
-#Q.6)
+#Q.6 )
 
 def isprime(x):
     for i in range(2, x):
@@ -42,7 +42,7 @@ li=[15,26,33,13,53,97,22]
 a = list(filter(isprime, li))
 print(a)
 
-#Q.7)
+#Q.7 )
 
 li=[5,50,15,20]
 from functools import *
